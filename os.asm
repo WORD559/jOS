fed2145f4d3e1296bcef2552e713d83d3fa4a050
WORD559 <bobbumder@gmail.com>
2017-07-22 16:13:08 +0100

    First booloader

---
+++ os.asm
@@ -0,0 +1,33 @@
+    BITS 16
+start:
+    mov ax, 07c0h           ; 4K stack space after the bootloader
+    add ax, 288             ; (4098 + 512)/16 bytes per paragraph
+    mov ss, ax
+    mov sp, 4096
+    
+    mov ax, 07c0h           ; set data segment to where we're loaded
+    mov ds, ax
+    
+    mov si, text_string     ; put string position into SI
+    call print_string       ; call print_string routine
+    
+    jmp $
+    
+    text_string db "Welcome to Bum'dOS v1", 0
+
+print_string:
+    mov ah, 0Eh             ; Put 0Eh in ah -- Tells BIOS to print character in AL at INT 10h
+    
+.repeat:
+    lodsb                   ; load a byte from the string in SI to AL
+    cmp al, 0               ; if AL == 0...
+    je .done                ; jump to done...
+    int 10h                 ; else call the BIOS interrupt to print AL...
+    jmp .repeat             ; and repeat.
+    
+.done:
+    ret                     ; return from CALL
+    
+
+    times 510-($-$$) db 0   ; fill the rest of the boot sector with 0s
+    dw 0xAA55               ; and add a boot sector signature.
