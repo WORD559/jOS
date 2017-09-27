adbaec3fc31b7eb25326d28f13359d3450de043b
WORD559 <josh.barrass.work@gmail.com>
2017-09-27 17:57:50 +0100

    Reduce file size by shortening strings

--- os.asm
+++ os.asm
@@ -32,7 +32,7 @@ start:
     
     jmp $
     
-    splash_text: db "==Welcome to Bum'dOS v3.2!==\n\n\F0The only OS you can truly call \F6b\F<u\F>m\F:t\F9i\F1n\F5g\F0.\n\nNow with FAT12 support for files greater than 512 bytes!", 0
+    splash_text: db "==Welcome to jOS v3.2!==\n\n\F0The only OS you can truly call \F6b\F<u\F>m\F:t\F9i\F1n\F5g\F0.\n\nNow with FAT12 support for files greater than 512 bytes!", 0
     cursor: dw 0x00
     colour: db 0x07
     
